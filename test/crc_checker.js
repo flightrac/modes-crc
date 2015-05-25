9fef74067ea1c526bf04a7e66e2f639db03d547f
Fabio Oliveira <fabio.an.oliveira@gmail.com>
2015-05-25 15:16:23 +0100

    Added method to calculate crc checksum of a given frame and polished code that does the crc parity check

--- test/crc_checker.js
+++ test/crc_checker.js
@@ -1,26 +1,36 @@
-var test = require('tape')
-var crc = require('../lib/checker')
+'use strict'
 
-test('valid crc for short message', function(t) {
+var test = require('tape')
+var checker = require('../lib/checker')
+
+test('valid checksum for short message', function(t) {
   var data = [ 93, 77, 32, 35, 122, 85, 166 ]
 
-  t.equal(crc.checksum(data, crc.SHORT_MSG_BITS), 0x7a55a6)
+  t.equal(checker.checksum(data, checker.SHORT_MSG_BITS), 0x7a55a6)
 
   t.end()
 })
 
-test('invalid crc for short message', function(t) {
+test('invalid checksum for short message', function(t) {
   var data = [ 40, 0, 16, 36, 220, 121, 76 ]
 
-  t.notEqual(crc.checksum(data, crc.SHORT_MSG_BITS), 0xdc794c)
+  t.notEqual(checker.checksum(data, checker.SHORT_MSG_BITS), 0xdc794c)
 
   t.end()
 })
 
-test('valid crc for long message', function(t) {
+test('valid checksum for long message', function(t) {
   var data = [ 143, 77, 32, 35, 88, 127, 52, 94, 53, 131, 126, 34, 24, 178]
 
-  t.equal(crc.checksum(data, crc.LONG_MSG_BITS), 0x2218b2)
+  t.equal(checker.checksum(data, checker.LONG_MSG_BITS), 0x2218b2)
+
+  t.end()
+})
+
+test('crc is as expected for any kind of data packet', function(t) {
+  var data = [ 40, 0, 16, 36, 220, 121, 76 ]
+
+  t.equal(checker.crc(data, checker.SHORT_MSG_BITS), 0xdc794c)
 
   t.end()
 })
